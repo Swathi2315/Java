fb359f00947e95002ecf059fcf345c8ab193c517
Swathyyyyy <swathim.xworkz@gmail.com>
2024-06-24 00:34:18 +0530

    This is my First commit

---
+++ Software.java
@@ -0,0 +1,14 @@
+public class Software {
+    public static void main(String[] args) {
+        
+        String software = "Visual Studio";
+		String birth    = "1997"
+		String uses     = "Windows forms"
+		String language = "C#, F#"
+        System.out.println("Software is"+software);
+        System.out.println("Birth is"+birth);
+        System.out.println("Uses are"+uses);
+        System.out.println("Languages are"+language);
+    
+    }
+}
