6700eeab3772a20c4e681b0d27ac690587345210
Swathyyyyy <swathim.xworkz@gmail.com>
2024-06-24 10:27:58 +0530

    Friday Assignment

---
+++ Exam.java
@@ -0,0 +1,17 @@
+public class Exam {
+    public static void main(String[] args) {
+        // Declare subject marks
+        int subject1 = 85;
+        int subject2 = 90;
+        int subject3 = 78;
+        int subject4 = 92;
+        int subject5 = 88;
+        int subject6 = 79;
+
+        // Calculate total score
+        int totalScore = subject1 + subject2 + subject3 + subject4 + subject5 + subject6;
+
+        // Display total score
+        System.out.println("Total Score: " + totalScore);
+    }
+}
---
+++ News.java
@@ -0,0 +1,29 @@
+public class News {
+    // Static variables
+    static int staticVar1 = 10;
+    static String staticVar2 = "Hello";
+    static double staticVar3 = 3.14;
+    static boolean staticVar4 = true;
+    static char staticVar5 = 'A';
+
+    public static void main(String[] args) {
+        // Local variables
+        int localVar1 = 20;
+        String localVar2 = "World";
+        double localVar3 = 2.71;
+
+        // Display static variables
+        System.out.println("Static Variables:");
+        System.out.println("staticVar1 = " + staticVar1);
+        System.out.println("staticVar2 = " + staticVar2);
+        System.out.println("staticVar3 = " + staticVar3);
+        System.out.println("staticVar4 = " + staticVar4);
+        System.out.println("staticVar5 = " + staticVar5);
+
+        // Display local variables
+        System.out.println("\nLocal Variables:");
+        System.out.println("localVar1 = " + localVar1);
+        System.out.println("localVar2 = " + localVar2);
+        System.out.println("localVar3 = " + localVar3);
+    }
+}
